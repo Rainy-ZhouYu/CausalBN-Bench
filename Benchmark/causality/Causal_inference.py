031e213fa41112bd23932765c7de8dc8661a8a54
Rainy-ZhouYu <148537589+Rainy-ZhouYu@users.noreply.github.com>
2024-09-08 23:43:55 +0800

    Causal_inference.py

--- Benchmark/causality/Causal_inference.py
+++ Benchmark/causality/Causal_inference.py
@@ -7,14 +7,13 @@ from transformers import AutoModelForCausalLM, AutoTokenizer
 import time
 
 
-# GPT 模型的处理函数
 def answer_for_gpt(questions, model, outputFileName):
     i = 0
     answers = []
     for question in questions:
         attempts = 0
         success = False
-        while attempts < 3 and not success:  # 尝试最多 3 次
+        while attempts < 3 and not success:  
             try:
                 response = openai.ChatCompletion.create(
                     model=f"{model}",
@@ -23,7 +22,7 @@ def answer_for_gpt(questions, model, outputFileName):
                          "content": "You are a highly intelligent question-answering bot with profound knowledge of causal inference."},
                         {"role": "user", "content": question}
                     ],
-                    max_tokens=20
+                    max_tokens=100
                 )
                 answer = response.choices[0].message['content'].strip()
                 answers.append(answer)
@@ -39,7 +38,7 @@ def answer_for_gpt(questions, model, outputFileName):
             except openai.error.OpenAIError as e:
                 print(f"An error occurred: {e}")
                 attempts += 1
-                time.sleep(5)  # 等待 5 秒再重试
+                time.sleep(5)  
 
         if not success:
             print(f"Failed to get an answer for the question: {question}")
@@ -47,7 +46,7 @@ def answer_for_gpt(questions, model, outputFileName):
     return answers
 
 
-# Huggingface 模型的处理函数
+
 def hf_main(locationLlamaHF, outputFileName, inputFileName):
     tokenizer = AutoTokenizer.from_pretrained(locationLlamaHF, cache_dir="~/cache/")
     model = AutoModelForCausalLM.from_pretrained(locationLlamaHF, device_map="auto", offload_folder="offload",
@@ -80,7 +79,6 @@ def hf_main(locationLlamaHF, outputFileName, inputFileName):
                 writer.writerows(outputs)
 
 
-# 生成 prompts 的辅助函数
 def generate_prompt(instruction: str, input_ctxt: str = None) -> str:
     if input_ctxt:
         return f"""Below is an instruction that describes a task, paired with an input that provides further context. Write a response that appropriately completes the request.
@@ -101,23 +99,22 @@ def generate_prompt(instruction: str, input_ctxt: str = None) -> str:
 ### Response:"""
 
 
-# 主函数，处理命令行参数
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Choose a model type: 'GPT' or 'Huggingface'")
     parser.add_argument('--model_type', type=str, required=True, help="Choose between 'GPT' or 'Huggingface'")
     args = parser.parse_args()
 
     if args.model_type == 'GPT':
-        openai.api_base = "https://api.gptapi.us/v1/"
-        openai.api_key = 'sk-zUi6sp9FanLQEPza4f0336B165E244579b687dFb8d564293'
+        openai.api_base = "opanai_url"
+        openai.api_key = 'your_key'
         models = ['gpt-3.5-turbo-1106']
         input_datasets = ["water", "hailfinder"]
 
         for model in models:
             Answer = []
             for input_dataset in input_datasets:
-                outputFileName = f'Response/Task_Causal_Process_Test/{model}_{input_dataset}.csv'
-                questions_df = pd.read_csv(f'generate_question/question/questions_{input_dataset}.csv')
+                outputFileName = f'/{model}_{input_dataset}.csv'
+                questions_df = pd.read_csv(f'questions_{input_dataset}.csv')
                 questions = questions_df['prompt'].tolist()
                 Answer = answer_for_gpt(questions, model, outputFileName)
 
@@ -125,13 +122,13 @@ if __name__ == '__main__':
                 qa_pairs.to_csv(f'Response/Task_Causal/{model}_{input_dataset}.csv', index=False)
 
     elif args.model_type == 'Huggingface':
-        models = ["OPT1d3b", "OPT2d7b", "OPT6d7b"]
-        input_files = [f"generate_question_correlation/question/questions_{region}.csv" for region in ["asia"]]
+        models = ["OPT1d3b", "OPT2d7b", "OPT6d7b"]#open LLMs
+        input_files = [f"questions_{region}.csv" for region in ["asia"]]
 
         for model in models:
             for input_file in input_files:
-                locationLlamaHF = f"G:/LLM/{model}"
-                outputFileName = f"Result/Task1/{model}_{input_file.split('/')[-1].split('.')[0]}.csv"
+                locationLlamaHF = f"{model}"
+                outputFileName = f"{model}_{input_file.split('/')[-1].split('.')[0]}.csv"
                 hf_main(locationLlamaHF, outputFileName, input_file)
     else:
         print("Invalid model type! Please choose either 'GPT' or 'Huggingface'.")
